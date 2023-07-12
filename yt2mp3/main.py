4151080b179bdf1df5b4570c670b26ff7e85cc8d
Sacha <106993611+sachagd@users.noreply.github.com>
2023-07-12 12:20:24 +0200

    Update main.py
    now multiple song can be donwloaded at the same time

--- yt2mp3/main.py
+++ yt2mp3/main.py
@@ -1,24 +1,30 @@
 import youtube_dl
+from multiprocessing import Pool
 
-input = open("input.txt",'r')
-playlist = input.read()
-
-ydl_opts = {
-    "ignoreerrors": True,
-    "quiet": True,
-    "extract_flat": "in_playlist",
-    "extract_flat_opts": {
-        "force_generic_extractor": True,
-        "in_playlist": True,
-        "video_select": "title,webpage_url"
-    }
-}
-
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    playlist_dict = ydl.extract_info(playlist, download=False)
-
-playlist_entries = playlist_dict.get("entries", [])
-
-for video in playlist_entries:
-    with youtube_dl.YoutubeDL({'format':'bestaudio/best', 'keepvideo':False, 'outtmpl': "output\\"+video['title']+".mp3"}) as ydl:
+def download_song(video):
+    with youtube_dl.YoutubeDL({'format': 'bestaudio/best', 'keepvideo': False,
+                               'outtmpl': "output\\" + video['title'].replace("/", " ") + ".mp3"}) as ydl:
         ydl.download([video['url']])
+
+def donwload(n): # Specify the number of processes you want to run concurrently
+    input_file = open("input.txt", 'r')
+    playlist_url = input_file.read()
+
+    ydl_opts = {
+        "ignoreerrors": True,
+        "quiet": True,
+        "extract_flat": "in_playlist",
+        "extract_flat_opts": {
+            "force_generic_extractor": True,
+            "in_playlist": True,
+            "video_select": "title,webpage_url"
+        }
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        playlist_dict = ydl.extract_info(playlist_url, download=False)
+
+    playlist_entries = playlist_dict.get("entries", [])
+
+    with Pool(n) as pool:
+        pool.map(download_song, playlist_entries)

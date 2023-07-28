5d23512eeecb231000b19a1aa8f5f688a25ae42a
Sacha <106993611+sachagd@users.noreply.github.com>
2023-07-28 21:56:16 +0200

    Update main.py

--- png2pdf/main.py
+++ png2pdf/main.py
@@ -1,19 +1,15 @@
 from PIL import Image
 import os
 
-png_folder = 'input'
-pdf_folder = 'output' 
+input_folder = 'input'
+output_folder = 'output' 
 
-
-for i in os.listdir(pdf_folder):
-    os.remove(os.path.join(pdf_folder,i))
-image = [Image.open(os.path.join(png_folder, i)).convert('RGB') for i in os.listdir(png_folder)]
-image[0].save(os.path.join(pdf_folder, 'output.pdf'), save_all=True, append_images=image[1:len(image)])
-
+image = [Image.open(os.path.join(inuput_folder, i)).convert('RGB') for i in os.listdir(input_folder)]
+image[0].save(os.path.join(output_folder, 'output.pdf'), save_all=True, append_images=image[1:len(image)])
 
 b = input("veux-tu supprimer les png ? (o/n)")
 for i in image:
     i.close()
 if b == "o":
-    for i in os.listdir(png_folder):
-        os.remove(os.path.join(png_folder,i))
+    for i in os.listdir(input_folder):
+        os.remove(os.path.join(input_folder,i))

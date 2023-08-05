48d28980fd2472cc17e1221c3d762ad7882596e8
Sacha <106993611+sachagd@users.noreply.github.com>
2023-08-05 22:17:52 +0200

    Update main.py
    funny mistake

--- png2pdf/main.py
+++ png2pdf/main.py
@@ -4,7 +4,7 @@ import os
 input_folder = 'input'
 output_folder = 'output' 
 
-image = [Image.open(os.path.join(inuput_folder, i)).convert('RGB') for i in os.listdir(input_folder)]
+image = [Image.open(os.path.join(input_folder, i)).convert('RGB') for i in os.listdir(input_folder)]
 image[0].save(os.path.join(output_folder, 'output.pdf'), save_all=True, append_images=image[1:len(image)])
 
 b = input("veux-tu supprimer les png ? (o/n)")

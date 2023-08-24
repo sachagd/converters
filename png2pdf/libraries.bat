5a13036a8a8be616d24f928e11caeffe2222cd85
Sacha <106993611+sachagd@users.noreply.github.com>
2023-08-24 18:44:05 +0200

    Create libraries.bat

---
+++ png2pdf/libraries.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM List of required libraries
+set "LIBRARIES=PyPDF2"
+
+REM Loop through each library and check if it's installed
+for %%i in (%LIBRARIES%) do (
+    python -c "import %%i" 2>nul
+    if errorlevel 1 (
+        echo Installing %%i...
+        pip install %%i
+    ) else (
+        echo %%i is already installed.
+    )
+)
+
+echo All required libraries have been checked and installed if needed.
+
+pause
+
+REM Delete the batch file
+del %0
